d679ab90794afeecbd973ec8bf872f4c360e86ca
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2023-06-14 11:44:24 +0300

    feat(Bootstrap): install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.png';
 import './App.css';
 
-function App() {
+function App() 
+{
   return (
     <div className="App">
+      <h1> Saji<span id='li-blue'>li</span></h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <div className='Container'>
+
+      </div>
     </div>
   );
 }
